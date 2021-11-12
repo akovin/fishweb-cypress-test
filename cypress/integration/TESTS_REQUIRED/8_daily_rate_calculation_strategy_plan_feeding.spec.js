17b868fa80be0c6adc7f75b0e3ecb3bef6463c8d
kovin <kovin@arvata.ru>
2021-11-12 17:33:21 +0300

    2 tests in 8 suite made

--- cypress/integration/TESTS_REQUIRED/8_daily_rate_calculation_strategy_plan_feeding.spec.js
+++ cypress/integration/TESTS_REQUIRED/8_daily_rate_calculation_strategy_plan_feeding.spec.js
@@ -10,11 +10,18 @@ describe("daily feeding rate calculation, strategy handbook", () => {
     cy.login()
     cy.removeAllTestIndicatorsConstantTank(Cypress.env('tankTestConstantId'), ['seeding', 'temperature'])
   })
-  it("daily feeding rate calculation, strategy handbook", () => {
+  it("daily feeding rate calculation, strategy feeding plan(without condition by fishweight)", () => {
     enterIndicators.seeding(Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), 3600, 370.8, '1 Авг 15:00')
     enterIndicators.temperature(Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), 14, '1 Авг 15:00')
     cy.fixture('feedingPlans').as('feedingPlans').then((feedingPlans) => {
         checkIndicators.checkFeeding('calculatedDataWithoutFillInFeed', Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), null, '9.4', '0.80', null, feedingPlans.find(feedingPlan => feedingPlan.name == Cypress.env('feedingPlanFirstName')).id, '2 Авг 15:00')
+    })
+  })
+  it.only("daily feeding rate calculation, strategy feeding plan(with condition by fishweight)", () => {
+    enterIndicators.seeding(Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), 3600, 72, '1 Авг 15:00')
+    enterIndicators.temperature(Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), 14, '1 Авг 15:00')
+    cy.fixture('feedingPlans').as('feedingPlans').then((feedingPlans) => {
+        checkIndicators.checkFeeding('calculatedDataWithoutFillInFeed', Cypress.env('siteTestConstantId'), Cypress.env('tankTestConstantId'), null, '3.0', '0.80', null, feedingPlans.find(feedingPlan => feedingPlan.name == Cypress.env('feedingPlanSecondName')).id, '3 Авг 15:00')
     })
   })
 })
